c7349895a86751e2f5827d6b52e970a2f0ac4d1c
Aziz <103178186+azizsigar@users.noreply.github.com>
2024-07-14 00:30:18 +0200

    added sound efect and prosses

--- quickmath/comps/Q1.jsx
+++ quickmath/comps/Q1.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Toaster, toast } from "react-hot-toast";
+import correctAnswerSound from "../media/pass.mp3"; // Ses dosyasını içe aktarın
+import errorSound from "../media/errorcombomaster.mp3"; // Ses dosyasını içe aktarın
 
 const Q1 = () => {
   const randomNum = () => {
-    let num = Math.floor(Math.random() * 10);
+    let num = Math.floor(Math.random() * 100);
     while (num === 0) {
       num = Math.floor(Math.random() * 10);
     }
@@ -13,6 +16,10 @@ const Q1 = () => {
   const [num2, setNum2] = useState(0);
   const [num3, setNum3] = useState(0);
   const [userInput, setUserInput] = useState(""); // State to hold user input
+  const [errorCount, setErrorCount] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0);
+  const correctAnswerAudio = useRef(new Audio(correctAnswerSound)); // Ses dosyasını oluşturun
+  const errorAudio = useRef(new Audio(errorSound)); 
 
   useEffect(() => {
     const n1 = randomNum();
@@ -20,6 +27,12 @@ const Q1 = () => {
     setNum1(n1);
     setNum2(n2);
     setNum3(n1 + n2);
+
+    // Local storage'dan doğru cevap sayısını al
+    const savedCorrectCount = localStorage.getItem("correctCount");
+    if (savedCorrectCount !== null) {
+      setCorrectCount(parseInt(savedCorrectCount));
+    }
   }, []);
 
   const handleChange = (event) => {
@@ -28,18 +41,49 @@ const Q1 = () => {
 
   const handleCheck = () => {
     if (parseInt(userInput) === num3) {
-      window.location.reload(); 
+      //toaster add
+      toast.success("Correct Answer");
+      correctAnswerAudio.current.play();
+      const newCorrectCount = correctCount + 1;
+      setCorrectCount(newCorrectCount);
+      localStorage.setItem("correctCount", newCorrectCount);
+      setTimeout(() => {
+        window.location.reload();
+      }, 1200);
     } else {
-      console.log("its white magic")
+      toast.error("Try again", {
+        duration: 500,
+      });
+      setCorrectCount(0);
+      localStorage.setItem("correctCount", 0);
+
+      const newErrorCount = errorCount + 1;
+      setErrorCount(newErrorCount); // Hatalı deneme sayısını artır
+      if (newErrorCount > 15) {
+        errorAudio.current.play(); 
+        toast.error("Are you an error combo master?", { duration: 6000, position: 'bottom-center' });
+      }
     }
   };
-console.log(num1, num2, num3)
+
+  console.log(num1, num2, num3);
   return (
     <div>
-      <div>{num1} + {num2} =</div>
-      <input type="text" value={userInput} onChange={handleChange} />
       <div>
-        <button className="button-74" onClick={handleCheck}>check</button>
+        <Toaster />
+      </div>
+      <div>
+        <h1>Quick Math</h1>
+        <h2>Combo: {correctCount}</h2>
+      </div>
+      <div>
+        {num1} + {num2} =
+      </div>
+      <input type="digit" value={userInput} onChange={handleChange} />
+      <div>
+        <button className="button-74" onClick={handleCheck}>
+          Check
+        </button>
       </div>
     </div>
   );
